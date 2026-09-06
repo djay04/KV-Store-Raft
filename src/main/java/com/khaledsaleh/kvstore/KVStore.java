c8b31d337a6835307f213678e8759553e888b7c5
Khaled <117534908+djay04@users.noreply.github.com>
2026-09-06 15:32:36 -0700

    feat: Completed implementation of KVStore --> includes get/set/delete operations on keys/values provided by ClientHandler handoff

--- src/main/java/com/khaledsaleh/kvstore/KVStore.java
+++ src/main/java/com/khaledsaleh/kvstore/KVStore.java
@@ -1,9 +1,47 @@
-package main.java.com.khaledsaleh.kvstore;
+package com.khaledsaleh.kvstore;
 
-
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 // HashMap + Locking Logic
 
 public class KVStore {
+    
+    private final ReentrantLock lock = new ReentrantLock();
+
+    private final HashMap<String, String> store = new HashMap<>();
+    
+    public void set(String key, String value) {
+        
+        lock.lock();
+
+        try {
+            store.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String get(String key) {
+        
+        lock.lock();
+
+        try {
+            return store.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void del(String key){
+
+        lock.lock();
+
+        try {
+        store.remove(key);
+        } finally {
+            lock.unlock();
+        }
+    }
     
 }
